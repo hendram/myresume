26ff2a4d80db2cbd68c3d0efba8cdb35e9d83135
root <root@debian.localhost>
2025-04-29 22:54:27 +0000

    Fix eslint error

--- components/Education.js
+++ components/Education.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import education from "../styles/education.module.css";
 import { edudata } from "@/app/lib/education.js";
 import { certification } from "@/app/lib/certification.js";
