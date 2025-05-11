22eb9716378e4bb782f7a75326f8c3e88cc4b032
root <root@debian.localhost>
2025-05-11 23:39:27 +0000

    fix color error

--- components/PortalTooltipportrait.js
+++ components/PortalTooltipportrait.js
@@ -6,7 +6,9 @@ export default function PortalTooltipportrait({ desc, style }) {
   return createPortal(
     <div style={{ position: "absolute", zIndex: 9999, ...style }}>
       <div className={portaltooltipportrait.tooltip} >
+        <div className={portaltooltipportrait.tooltipbox} >
         {desc}
+        </div>
       </div>
     </div>,
     document.body
