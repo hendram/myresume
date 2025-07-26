986c69c65033ebbaa8973f55b64d3621bb40be99
hendra <hendra_mulyanto@yahoo.com>
2025-07-26 12:37:32 +0000

    fix cert

--- app/lib/certification.js
+++ app/lib/certification.js
@@ -1,8 +1,7 @@
 export const certification = {
-     networking: "https://www.scribd.com/document/890518625/Hendra-Mulyanto-Cisco-CCNP-Certificate",
-     ethical_hacking: "https://www.scribd.com/document/890517824/Hendra-Mulyanto-CEH-Certificate",
-     cloud:" https://www.credly.com/badges/6b129086-bf8d-4b71-8305-07c0df43f918/public_url",
-     kubernetes: "https://www.scribd.com/document/893258627/Hendra-Mulyanto-Calico-Level-1-Certificate" 
+     networking: "ccna, ccnp",
+     ethical_hacking: "ceh",
+     kubernetes: "calico" 
 }
 
 
